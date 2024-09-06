868a1ed799f7fb165417c4d2ab090fe795ee28c3
jemss31 <jamessvenansali@gmail.com>
2024-09-06 21:13:28 +0800

    Delete getsalary.java

--- getsalary.java
+++
@@ -1,9 +0,0 @@
-
-package employee;
-
-public class getsalary {
-    public static void main(String[] args) {
-        Salary salaryCalculator = new Salary();
-        salaryCalculator.getSalary();
-    }
-}
