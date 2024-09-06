09a2e0425c35c0ca07cc27f88b6af655e1dec480
jemss31 <jamessvenansali@gmail.com>
2024-09-06 21:14:04 +0800

    Add files via upload

---
+++ employee/getsalary.java
@@ -0,0 +1,9 @@
+
+package employee;
+
+public class getsalary {
+    public static void main(String[] args) {
+        Salary salaryCalculator = new Salary();
+        salaryCalculator.getSalary();
+    }
+}
